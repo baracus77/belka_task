c331327f8b228929de7ed8d9e7c21418c8ce2368
Alexander Perednya <alexander_perednya@belkatechnologies.com>
2021-06-01 13:20:07 +0300

    Add gitignore

--- Test__task.py
+++ Test__task.py
@@ -15,49 +15,49 @@ class RoomAction():
         self.command = ""
         while self.command != self.quit:
             self.command = str.lower(input())
-
-            if self.command in self.door_action: #открываем дверь
-                if self.command == self.door_action[0] == self.door: #дверь открыта
-                    print("Дверь уже открыта")
-                elif self.command == self.door_action[0] != self.door: #дверь закрыта
-                    if self.lock == self.lock_action[0]: #замок открыт
-                        self.door = self.command
-                        print("Вы открыли дверь")
-                    else: #замок закрыт
-                        print("Дверь открыть нельзя, заперта на замок")
-                elif self.command == self.door_action[1] == self.door: #дверь закрыта
-                    print("Дверь уже закрыта")
-                elif self.command == self.door_action[1] != self.door:
-                    if self.lock == self.lock_action[0]: #замок открыт
-                        self.door = self.command
-                        print("Вы закрыли дверь")
-                    else: #замок закрыт
+            try:
+                if self.command in self.door_action: #открываем дверь
+                    if self.command == self.door_action[0] == self.door: #дверь открыта
+                        print("Дверь уже открыта")
+                    elif self.command == self.door_action[0] != self.door: #дверь закрыта
+                        if self.lock == self.lock_action[0]: #замок открыт
+                            self.door = self.command
+                            print("Вы открыли дверь")
+                        else: #замок закрыт
+                            print("Дверь открыть нельзя, заперта на замок")
+                    elif self.command == self.door_action[1] == self.door: #дверь закрыта
                         print("Дверь уже закрыта")
+                    elif self.command == self.door_action[1] != self.door:
+                        if self.lock == self.lock_action[0]: #замок открыт
+                            self.door = self.command
+                            print("Вы закрыли дверь")
+                        else: #замок закрыт
+                            print("Дверь уже закрыта")
 
-            elif self.command in self.lock_action: #открываем замок
-                if self.command == self.lock_action[0]: #команда отпереть
-                    if self.door == self.door_action[0]: #дверь открыта
-                        print("Открытую дверь нельзя отпереть")
-                    elif self.door == self.door_action[1]: #дверь закрыта
-                        if self.lock == self.lock_action[0]: #замок отперт
-                            print("Замок уже отперт")
-                        else:
-                            self.lock = self.command
-                            print("Вы отперли замок")
-                elif self.command == self.lock_action[1]: #команда запереть
-                    if self.door == self.door_action[0]: #дверь открыта
-                        print("Закрой дверь, чтобы Запереть замок")
+                elif self.command in self.lock_action: #открываем замок
+                    if self.command == self.lock_action[0]: #команда отпереть
+                        if self.door == self.door_action[0]: #дверь открыта
+                            print("Открытую дверь нельзя отпереть")
+                        elif self.door == self.door_action[1]: #дверь закрыта
+                            if self.lock == self.lock_action[0]: #замок отперт
+                                print("Замок уже отперт")
+                            else:
+                                self.lock = self.command
+                                print("Вы отперли замок")
+                    elif self.command == self.lock_action[1]: #команда запереть
+                        if self.door == self.door_action[0]: #дверь открыта
+                            print("Закрой дверь, чтобы Запереть замок")
 
-                    elif self.door == self.door_action[1]: #дверь закрыта
-                        if self.lock == self.lock_action[0]: #замок отперт
-                            self.lock = self.command
-                            print("Вы заперли замок")
-                        else:
-                            print("Замок уже заперт")
-            elif self.command != self.quit :
-                raise print("Введена не верная команда для двери или замка")
-
-
+                        elif self.door == self.door_action[1]: #дверь закрыта
+                            if self.lock == self.lock_action[0]: #замок отперт
+                                self.lock = self.command
+                                print("Вы заперли замок")
+                            else:
+                                print("Замок уже заперт")
+                elif self.command != self.quit:
+                    raise Exception("Введена не верная команда для двери или замка")
+            except Exception as error_input:
+                print(error_input)
 
 room_class = RoomAction()
 room_class.door_default_state(room_class.door_action[0],room_class.lock_action[0], room_class.quit)
